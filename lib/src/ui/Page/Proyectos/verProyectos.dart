749ab83a76d14bdecade2a02b220b768bd7416c0
Zeustk <112997729+Zeustk@users.noreply.github.com>
2024-06-09 01:00:24 -0500

    tt

--- lib/src/ui/Page/Proyectos/verProyectos.dart
+++ lib/src/ui/Page/Proyectos/verProyectos.dart
@@ -47,9 +47,8 @@ class _Ver_ProyectosState extends State<Ver_Proyectos> {
     try {
       UsuarioModel usuarioActualinfo =
           await gestionAuth.obtenerDatosDeStorage();
-      List<Map<String, dynamic>> proyectosByLiderActual =
-          await gestionDetalles.obtenerProyectosLiderPorUsuario(
-              usuarioActualinfo.idUsuario);
+      List<Map<String, dynamic>> proyectosByLiderActual = await gestionDetalles
+          .obtenerProyectosLiderPorUsuario(usuarioActualinfo.idUsuario);
 
       print(proyectosByLiderActual);
 
@@ -115,10 +114,7 @@ class _Ver_ProyectosState extends State<Ver_Proyectos> {
   }
 
   void _showConfirmationDialog(BuildContext context, int idProyecto) {
-  
-
     bool siUsuarioEsLider = esLiderProyecto(idProyecto);
-
 
     if (siUsuarioEsLider) {
       showDialog(
@@ -149,12 +145,12 @@ class _Ver_ProyectosState extends State<Ver_Proyectos> {
         },
       );
     } else {
-       Get.snackbar(
-          'Pemiso Denegado',
-          'Solo el Lider del Proyecto Puede Eliminar Proyectos',
-          backgroundColor: Colors.white,
-          colorText: Colors.black,
-        );
+      Get.snackbar(
+        'Pemiso Denegado',
+        'Solo el Lider del Proyecto Puede Eliminar Proyectos',
+        backgroundColor: Colors.white,
+        colorText: Colors.black,
+      );
     }
   }
 
@@ -192,10 +188,9 @@ class _Ver_ProyectosState extends State<Ver_Proyectos> {
     });
   }
 
-  bool esLiderProyecto(int idProyecto){
-
-    return  proyectosDeLiderActual.any((proyecto) {
-        print(proyecto["Id_Proyecto"]);
+  bool esLiderProyecto(int idProyecto) {
+    return proyectosDeLiderActual.any((proyecto) {
+      print(proyecto["Id_Proyecto"]);
       return proyecto["Id_Proyecto"] == idProyecto;
     });
   }
@@ -306,20 +301,38 @@ class _Ver_ProyectosState extends State<Ver_Proyectos> {
                                         onTap: () {
                                           if (tipo[0] == 'M') {
                                             Get.to(DetalleProyectoPage(),
-                                                arguments:{
-                                                  "Lider_Proyecto":proyecto.liderProyecto,
-                                                  "Nombre":proyecto.nombre,
-                                                  "Fecha_Inicio":proyecto.fechaInicio,
-                                                  "Fecha_Finalizacion":proyecto.fechaFinalizacion,
-                                                  "Id_Proyecto":proyecto.idProyecto,
-                                                  "Id_Estado":proyecto.idEstado,
-                                                  "Porcentaje_Proyecto":proyecto.porcentajeProyecto,
-                                                  "Id_LiderProyecto": esLiderProyecto(proyecto.idProyecto),
-                                                  "Descripcion":proyecto.descripcion,
+                                                arguments: {
+                                                  "Lider_Proyecto":
+                                                      proyecto.liderProyecto,
+                                                  "Nombre": proyecto.nombre,
+                                                  "Fecha_Inicio":
+                                                      proyecto.fechaInicio,
+                                                  "Fecha_Finalizacion": proyecto
+                                                      .fechaFinalizacion,
+                                                  "Id_Proyecto":
+                                                      proyecto.idProyecto,
+                                                  "Id_Estado":
+                                                      proyecto.idEstado,
+                                                  "Porcentaje_Proyecto":
+                                                      proyecto
+                                                          .porcentajeProyecto,
+                                                  "Id_LiderProyecto":
+                                                      esLiderProyecto(
+                                                          proyecto.idProyecto),
+                                                  "Descripcion":
+                                                      proyecto.descripcion,
                                                 });
                                           } else {
-                                            Get.to(Ver_Tareas(),
-                                                arguments: proyecto.idProyecto);
+                                            Get.to(
+                                              Ver_Tareas(),
+                                              arguments: {
+                                                "Id_Proyecto":
+                                                    proyecto.idProyecto,
+                                                "Id_LiderProyecto":
+                                                    esLiderProyecto(
+                                                        proyecto.idProyecto),
+                                              },
+                                            );
                                           }
 
                                           print(
